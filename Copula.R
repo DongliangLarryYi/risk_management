e4b653d29cb1fc533baa742a5d7c640455944578
Dongliang "Larry" Yi <larry.ydl@gmail.com>
2016-08-28 14:23:52 -0500

    Add files via upload

---
+++ Copula.R
@@ -0,0 +1,280 @@
+#1.1 Independent situation
+set.seed(1)
+Trials = 200000
+Total_Loss = rep(0,Trials)
+Total_Loss_lambda = rep(0,Trials)
+for (i in 1:Trials){
+# loan: 10 Millions each
+RV1 = rnorm(80)
+U1 = pnorm(RV1)
+loss1 = 10 * length(U1[U1<0.02])
+
+# loan: 20 Millions each
+RV2 = rnorm(20)
+U2 = pnorm(RV2)
+loss2 = 20 * length(U2[U2<0.02])
+
+Total_Loss[i] = (loss1+loss2)/(10*80+20*20)
+
+#standard exponential
+RV1 = rnorm(80)
+U1 = pnorm(RV1)
+tao1 = -log(1-U1)/0.02;
+loss1 = 10 * length(tao1[tao1<1])
+
+RV2 = rnorm(20)
+U2 = pnorm(RV2)
+tao2 = -log(1-U2)/0.02;
+loss2 = 20 * length(tao2[tao2<1])
+Total_Loss_lambda[i] = (loss1+loss2)/(10*80+20*20)
+
+###############
+
+
+}
+h = hist(Total_Loss,breaks = 40, plot= FALSE)
+h$counts=h$counts/sum(h$counts)
+plot(h, col = "gray")
+
+h_lambda = hist(Total_Loss_lambda,breaks = 40, plot= FALSE)
+h_lambda$counts=h_lambda$counts/sum(h_lambda$counts)
+plot(h_lambda, col = "gray")
+
+mean(Total_Loss)
+mean(Total_Loss_lambda)
+
+
+#1.2
+set.seed(1)
+Total_Loss2 = rep(0,Trials)
+for (i in 1:Trials){
+  m = rnorm(1)
+  rho = 0.3
+  RV = sqrt(rho)*m + sqrt(1-rho)*rnorm(100)
+  RV
+  U = pnorm(RV)
+  U1 = U[1:80]
+  U2 = U[81:100]
+  
+  loss1 = 10 * length(U1[U1<0.02])
+  loss2 = 20 * length(U2[U2<0.02])
+  
+  Total_Loss2[i] = (loss1+loss2)/(10*80+20*20)
+}
+h2 = hist(Total_Loss2,breaks = 200, plot= FALSE)
+h2$counts=h2$counts/sum(h2$counts)
+plot(h2, col = "gray",xlim = c(0,0.1))
+
+#1.3
+set.seed(1)
+Trials3=500000
+sequence_A = rep(0,Trials3)
+sequence_B = rep(0,Trials3)
+rho = 0.3
+for (i in 1:Trials3){
+m = rnorm(1)
+RV = sqrt(rho)*m + sqrt(1-rho)*rnorm(2)
+RV = pnorm(RV)
+if (RV[1] > 0.98)
+  RV[1] = 1
+if (RV[1] <= 0.98)
+  RV[1] = 0
+
+if (RV[2] > 0.98)
+  RV[2] = 1
+if (RV[2] <= 0.98)
+  RV[2] = 0
+
+sequence_A[i] = RV[1]
+sequence_B[i] = RV[2]
+}
+#mean(sequence_B)
+cov(sequence_B,sequence_A)/sqrt(var(sequence_B)*var(sequence_A))
+
+#1.4
+set.seed(1)
+Trials3=200000
+sequence_A = rep(0,Trials3)
+sequence_B = rep(0,Trials3)
+cal_cor = rep(0,10)
+for (k in 1:10){
+rho = 0.20+ k*0.01
+for (i in 1:Trials3){
+  m = rnorm(1)
+  RV = sqrt(rho)*m + sqrt(1-rho)*rnorm(2)
+  RV = pnorm(RV)
+  if (RV[1] > 0.98)
+    RV[1] = 1
+  if (RV[1] <= 0.98)
+    RV[1] = 0
+  
+  if (RV[2] > 0.98)
+    RV[2] = 1
+  if (RV[2] <= 0.98)
+    RV[2] = 0
+  
+  sequence_A[i] = RV[1]
+  sequence_B[i] = RV[2]
+}
+#mean(sequence_B)
+cal_cor[k] = cov(sequence_B,sequence_A)/sqrt(var(sequence_B)*var(sequence_A))
+}
+
+#2.1
+EL1 = mean(Total_Loss)*(80*10+20*20)
+Q999_1 = (unname(quantile(Total_Loss,0.999,na.rm=T)))*(80*10+20*20)
+EC1 = Q999_1 - EL1
+EC1
+
+#2.2
+EL2 = mean(Total_Loss2)*(80*10+20*20)
+Q999_2 = (unname(quantile(Total_Loss2,0.999,na.rm=T)))*(80*10+20*20)
+EC2 = Q999_2 - EL2
+EC2
+
+#3.1
+set.seed(1)
+Total_Loss3 = rep(0,Trials)
+for (i in 1:Trials){
+  # loan: 10 Millions each
+  RV1 = rnorm(80)
+  U1 = pnorm(RV1)
+  loss1 = 10 * length(U1[U1<0.02])
+  
+  
+  # loan: 20 Millions each
+  RV2 = rnorm(20)
+  U2 = pnorm(RV2)
+  
+  if(length(U2[U2<0.02]) >= 4)
+    loss2 = 20* (length(U2[U2<0.02])-2)
+  
+  if(length(U2[U2<0.02]) < 4 & length(U2[U2<0.02]) > 0)
+    loss2 = 20 * (length(U2[U2<0.02]))/2
+  
+  if(length(U2[U2<0.02]) == 0)
+    loss2 = 0
+  Total_Loss3[i] = (loss1+loss2)/(10*80+20*20)
+}
+h3 = hist(Total_Loss3,breaks = 40, plot= FALSE)
+h3$counts=h3$counts/sum(h3$counts)
+plot(h3, col = "gray")
+
+#3.2
+EL3 = mean(Total_Loss3)*(80*10+20*20)
+Q999_3 = (unname(quantile(Total_Loss3,0.999,na.rm=T)))*(80*10+20*20)
+EC3 = Q999_3 - EL3
+EL3
+EC3
+
+#3.3
+set.seed(1)
+Total_Loss4 = rep(0,Trials)
+for (i in 1:Trials){
+  m = rnorm(1)
+  rho = 0.3
+  RV = sqrt(rho)*m + sqrt(1-rho)*rnorm(100)
+  RV
+  U = pnorm(RV)
+  U1 = U[1:80]
+  U2 = U[81:100]
+  
+  loss1 = 10 * length(U1[U1<0.02])  
+  
+  
+  if(length(U2[U2<0.02]) >= 4)
+    loss2 = 20* (length(U2[U2<0.02])-2)
+  
+  if(length(U2[U2<0.02]) < 4)
+    loss2 = 20 * (length(U2[U2<0.02]))/2
+
+  
+  Total_Loss4[i] = (loss1+loss2)/(10*80+20*20)
+}
+h4 = hist(Total_Loss4,breaks = 200, plot= FALSE)
+h4$counts=h4$counts/sum(h4$counts)
+plot(h4, col = "gray",xlim = c(0,0.1))
+EL4 = mean(Total_Loss4)*(80*10+20*20)
+Q999_4 = (unname(quantile(Total_Loss4,0.999,na.rm=T)))*(80*10+20*20)
+EC4 = Q999_4 - EL4
+EL4
+EC4
+
+#4.1
+set.seed(1)
+Total_Loss5 = rep(0,Trials)
+for (i in 1:Trials){
+  # loan: 10 Millions each
+
+  RV1 = rnorm(80)
+  U1 = pnorm(RV1)
+  
+  beta1 = rbeta(length(U1[U1<0.02]),2,2)
+  loss_beta1 = 1-beta1
+#  loss_ration = sum(loss_beta)/length(U1[U1<0.02])
+
+  
+  loss1 = 10 * sum(loss_beta1)
+  
+  
+  # loan: 20 Millions each
+  RV2 = rnorm(20)
+  U2 = pnorm(RV2)
+  
+  if (length(U2[U2<0.02]) >0){
+  beta2=rbeta(length(U2[U2<0.02]),2,2)
+  loss_beta2 = 1-beta2}
+  
+  if (length(U2[U2<0.02]) ==0)
+    loss_beta2 = 0
+  
+    
+    
+  if(sum(loss_beta2) >= 4)
+    loss2 = 20* (sum(loss_beta2)-2)
+  
+  if(sum(loss_beta2) < 4)
+    loss2 = 20 * (sum(loss_beta2))/2
+  
+  Total_Loss5[i] = (loss1+loss2)/(10*80+20*20)
+}
+h5 = hist(Total_Loss5,breaks = 40, plot= FALSE)
+h5$counts=h5$counts/sum(h5$counts)
+plot(h5, col = "gray")
+
+#4.2
+set.seed(1)
+Total_Loss6 = rep(0,Trials)
+for (i in 1:Trials){
+  m = rnorm(1)
+  rho = 0.3
+  RV = sqrt(rho)*m + sqrt(1-rho)*rnorm(100)
+  RV
+  U = pnorm(RV)
+  U1 = U[1:80]
+  U2 = U[81:100]
+  
+  beta1 = rbeta(length(U1[U1<0.02]),2,2)
+  loss_beta1 = 1-beta1
+  loss1 = 10 * sum(loss_beta1)
+  
+  
+  if (length(U2[U2<0.02]) >0){
+    beta2=rbeta(length(U2[U2<0.02]),2,2)
+    loss_beta2 = 1-beta2}
+  
+  if (length(U2[U2<0.02]) ==0)
+    loss_beta2 = 0
+  
+  if(sum(loss_beta2) >= 4)
+    loss2 = 20* (sum(loss_beta2)-2)
+  
+  if(sum(loss_beta2) < 4)
+    loss2 = 20 * (sum(loss_beta2))/2
+  
+  
+  Total_Loss6[i] = (loss1+loss2)/(10*80+20*20)
+}
+h6 = hist(Total_Loss6,breaks = 120, plot= FALSE)
+h6$counts=h6$counts/sum(h6$counts)
+plot(h6, col = "gray",xlim = c(0,0.1))
---
+++ DCC - new.R
@@ -0,0 +1,194 @@
+Data = read.csv("/Users/Larry/Documents/R_Code/DCC/HW8.csv")
+Return_IWM = log(Data$IWM.P[2:nrow(Data)] / Data$IWM.P[1:(nrow(Data)-1)])
+Return_EFA = log(Data$EFA.P[2:nrow(Data)] / Data$EFA.P[1:(nrow(Data)-1)])
+
+library(foreach)
+
+t = length(Return_IWM)
+
+Loglikelihood <- function(Return, Theta)
+{
+  if ( sum(Theta[1:4] <0) != 0 || Theta[1] * (1+Theta[5]^2) + Theta[2] > 1)
+  {
+    return (10000)
+    
+  }
+  t = length(Return)
+  Var = rep(0,t)
+  Var[1] = Theta[4]^2
+  
+  for(i in 2:t)
+  {
+    Var[i] = (1- Theta[1] * (1+ Theta[5]^2) - Theta[2]) * Theta[3]^2 + Theta[1] * (Return[i-1] - Theta[5] * sqrt(Var[i-1]))^2 + Theta[2] * Var[i-1]
+    
+  }
+  
+  LN = 0.5* sum(log(Var)+Return^2/Var)
+  return(LN)
+  
+}
+
+iIWM = c(0.1,0.88,sd(Return_IWM),sd(Return_IWM),0)
+result_IWM = optim(iIWM, Loglikelihood, Return=Return_IWM, method = "BFGS")
+result_IWM
+iEFA = c(0.08, 0.91, sd(Return_EFA), sd(Return_EFA),0)
+result_EFA = optim(iEFA, Loglikelihood, Return = Return_EFA, method = "BFGS")
+result_EFA
+Par_IWM = result_IWM$par
+Par_EFA = result_EFA$par
+
+DCCLN <- function(Return1, Return2, Theta){
+  if (Theta[1]<=0 || Theta[2]<=0 || Theta[1]+Theta[2]>1)
+  {return(10000)}
+  
+  t = length(Return1)
+  rho12_hist = mean(Return1*Return2)
+  
+  Q11=rep(0,t)
+  Q12=rep(0,t)
+  Q22=rep(0,t)
+  
+  Q11[1]=1
+  Q12[1]=rho12_hist
+  Q22[1]=1
+  
+  for (i in 2:t){
+    Q11[i] = 1 + Theta[1] * (Return1[i-1]^2 - 1) + Theta[2] * (Q11[i-1] - 1)
+    Q12[i] = rho12_hist + Theta[1] * (Return1[i-1] * Return2[i-1] - rho12_hist) + Theta[2] * (Q12[i-1] - rho12_hist)
+    Q22[i] = 1 + Theta[1] * (Return2[i-1]^2-1)+ Theta[2] * (Q22[i-1]-1)
+    
+  }
+  
+  rho12 = Q12/sqrt(Q11*Q22)
+  temp = 1 - rho12^2
+  LN = 0.5* sum(log(temp)+(Return1^2 + Return2^2 - 2 * rho12 * Return1*Return2) / temp)
+  
+  return(LN)
+  
+}
+
+#Standadizing
+z_IWM = rep(0,t)
+z_EFA = rep(0,t)
+Vol_IWM = rep(0,t)
+Vol_EFA = rep(0,t)
+Vol_IWM[1] = Par_IWM[4]
+Vol_EFA[1] = Par_EFA[4]
+for (i in 2:t){
+  Vol_IWM[i] = sqrt((1 - Par_IWM[1] *(1+ Par_IWM[5]^2)-Par_IWM[2]) * Par_IWM[3]^2 + Par_IWM[1] * (Return_IWM[i-1] - Par_IWM[5] * Vol_IWM[i-1])^2 + Par_IWM[2] * Vol_IWM[i-1]^2)
+  Vol_EFA[i] = sqrt((1 - Par_EFA[1] *(1+ Par_EFA[5]^2)-Par_EFA[2]) * Par_EFA[3]^2 + Par_EFA[1] * (Return_EFA[i-1] - Par_EFA[5] * Vol_EFA[i-1])^2 + Par_EFA[2] * Vol_EFA[i-1]^2)
+  
+}
+
+z_IWM = Return_IWM/Vol_IWM
+z_EFA = Return_EFA/Vol_EFA
+length(Vol_IWM)
+
+#DCC MLE
+iDCC = c(0.05, 0.9)
+result_DCC = optim(iDCC, DCCLN, Return1= z_IWM, Return2 = z_EFA, method = "BFGS")
+Par_DCC = result_DCC$par
+result_DCC
+
+#DCC of Rho
+hist_rho = mean(z_IWM*z_EFA)
+q11=1
+q12=hist_rho
+q22=1
+for (i in 2:t){
+  q11 = 1 + Par_DCC[1] * (z_IWM[i-1]^2-1) + Par_DCC[2]*(q11-1)
+  q12 = hist_rho + Par_DCC[1]*(z_IWM[i-1]*z_EFA[i-1]-hist_rho) + Par_DCC[2] * (q12 - hist_rho)
+  q22 = 1 + Par_DCC[1] * (z_EFA[i-1]^2-1) + Par_EFA[2]*(q22-1)
+}
+
+rho12 = q12/sqrt(q11*q22)
+
+#Monte Carlo
+
+#Working days number
+WD = 256
+
+# Price of Aug. 5 2014
+IWM_ini = Data$IWM.P[t+1]
+EFA_ini = Data$EFA.P[t+1]
+
+MCs = foreach(i = 1:5000) %do%
+{
+  
+  D = diag(c(Vol_IWM[t],Vol_EFA[t]))
+  Upsilon = matrix(c(1,rho12,rho12,1),2,2)
+  QQ11 = q11
+  QQ12 = q12
+  QQ22 = q22
+  
+  acumu_IWM = rep(0,WD)
+  acumu_EFA = rep(0,WD)
+  
+  acumu_IWM[1]=Return_IWM[t]
+  acumu_EFA[1]=Return_EFA[t]
+  
+  for (i in 2:WD)
+  {
+    rho = Upsilon[1,2]
+    cholesky_Upsilon = matrix(c(1,rho, 0, sqrt(1-rho^2)),2,2)
+    
+    r1 = acumu_IWM[i-1]
+    r2 = acumu_EFA[i-1]
+    v1 = D[1,1]
+    v2 = D[2,2]
+    
+    z1 = r1/v1
+    z2 = r2/v2
+    
+    # two models prediction
+    v1 = sqrt((1-Par_IWM[1] * (1 + Par_IWM[5]^2) - Par_IWM[2]) * Par_IWM[3]^2 + Par_IWM[1] * (r1 - Par_IWM[5] * v1)^2 + Par_IWM[2] * v1^2)
+    v2 = sqrt((1-Par_EFA[1] * (1 + Par_EFA[5]^2) - Par_EFA[2]) * Par_EFA[3]^2 + Par_EFA[1] * (r2 - Par_EFA[5] * v2)^2 + Par_EFA[2] * v2^2)
+    
+    QQ11 = 1 + Par_DCC[1] * (z1^2 -1) + Par_DCC[2] *(QQ11 -1)
+    QQ12 = hist_rho + Par_DCC[1]*(z1*z2 - hist_rho) + Par_DCC[2]*(QQ12 - hist_rho)
+    QQ22 = 1 + Par_DCC[1] * (z2^2 -1) + Par_DCC[2] *(QQ22 -1)    
+    rho = QQ12/sqrt(QQ11*QQ22)
+    
+    D = diag(c(v1,v2))
+    Upsilon = matrix(c(1,rho,rho,1),2,2)
+    
+    r= D %*% cholesky_Upsilon %*% rnorm(2)
+    
+    acumu_IWM[i] = r[1]
+    acumu_EFA[i] = r[2]
+    
+  }
+    Estimate_IWM = IWM_ini * exp(sum(acumu_IWM))
+    Estimate_EFA = EFA_ini * exp(sum(acumu_EFA))
+    
+  #Butters
+    Butter_IWM = (IWM_ini-Estimate_IWM)/IWM_ini
+    Butter_EFA = (EFA_ini-Estimate_EFA)/EFA_ini
+    
+    Worst = max(Butter_EFA,Butter_IWM)
+    
+    if (Worst <= 0.15)
+    {
+      P=1000
+    }
+    else
+    {
+      P = 1000 - 1000* (Worst -0.15) * 1.1765
+    }
+  
+  return (P)
+  
+}
+
+#Pricing, discount of each coupon payment, 12 month libor : 0.0056
+PV_coupon = 0
+MCs = unlist(MCs)
+
+for (i in 1:12){
+  PV_coupon = PV_coupon + 4.1833/(1+0.0056*(3+i*30)/360)
+  }
+
+Value_Present = PV_coupon + mean(MCs)/(1+0.0056*370/360)
+
+Value_Present
+
