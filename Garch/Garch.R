031ee8f6b902c1efef0ae988e47140a1dc38814b
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-27 22:16:08 -0500

    Update Garch.R

--- Garch/Garch.R
+++ Garch/Garch.R
@@ -1,48 +1,47 @@
-  library(fGarch)
+# This code fit Garch and NGarch models with historical S&P returns
 
+library(fGarch)
+# import S&P 500 historical price
 SP500 = read.csv("/Users/Larry/Documents/R_Code/Garch/SP500.csv")
-
 Index= SP500$AdjClose
-
+# calculate log return of S&P 500
 Return = log(Index[3:1002]/Index[2:1001])
 
-Return
-
+# use garchFit to fit a model which can be used for comparison with our estimated parameters under MLE
 mdl = garchFit(formula = ~ garch(1, 1), data = Return)
 summary(mdl)
 
-#estimating parameters of GARCH, four different methods as below. Detals please see problems set description.
-#1 
+#estimating parameters of GARCH, four different methods as below.
+#1: Four parameters to estimate
+# this is sum of pdf, which will be used to optimize
 sum_pdf <- function(theta)
   {
-
-  
-  var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]#sigma is long term variance
-  var_fore=rep(0,1000)
-  var_fore[1]=var1
-  for (i in 2:1000)
+    var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]#sigma is long term variance
+    var_fore=rep(0,1000)
+    var_fore[1]=var1
+    for (i in 2:1000)
   {
     var_fore[i] = long_var*(1-alpha-beta)+alpha*Return[i-1]^2+beta*var_fore[i-1]
   }
   sum = 0
   for (i in 1:1000)
-  {sum = sum - 0.5*(log(var_fore[i]*2*pi)+Return[i]^2/var_fore[i])}
-  
+  {
+    sum = sum - 0.5*(log(var_fore[i]*2*pi)+Return[i]^2/var_fore[i])
+  }
   return (-sum)
   }
-
-
+# initial value
 initialvalue=c(9.614052e-05,0.5,0.2,9.614052e-05)
-
+# optimization process
 result1 = optim(initialvalue, fn=function(theta){sum_pdf(theta)})
-
+# show result
 sqrt(result1$par[1])
 result1$par[2]
 result1$par[3]
 sqrt(result1$par[4])
 result1
 
-#2
+#2: Three parameters to estimate. Set long-run variance equalt to sample variance over the last 1000 days.
 longtermvar=var(Return)
 sum_pdf <- function(Return,theta)
 {
@@ -56,24 +55,24 @@ sum_pdf <- function(Return,theta)
   sum = 0
   for (i in 1:1000)
   {sum = sum - 0.5*(log(var_fore[i]*2*pi)+Return[i]^2/var_fore[i])}
-  
   return (-sum)
 }
+# initial value
 initialvalue=c(9.614052e-05,0.5,0.2)
+# optimization process
 result2 = optim(initialvalue, fn=function(theta){sum_pdf(Return,theta)})
-
+# show result
 sqrt(result2$par[1])
 result2$par[2]
 result2$par[3]
 sqrt(longtermvar)
-
 result2
 
-#3
-longtermvar=var(Return)
+#3: Three parameters to estimate. Set initial variance equalt to sample variance over the last 1000 days.
+initialvar=var(Return)
 sum_pdf <- function(Return,theta)
 {
-  long_var=theta[1];alpha=theta[2];beta=theta[3];var1=longtermvar#sigma is long term variance
+  long_var=theta[1];alpha=theta[2];beta=theta[3];var1=initialvar
   var_fore=rep(0,1000)
   var_fore[1]=var1
   for (i in 2:1000)
@@ -83,24 +82,23 @@ sum_pdf <- function(Return,theta)
   sum = 0
   for (i in 1:1000)
   {sum = sum - 0.5*(log(var_fore[i]*2*pi)+Return[i]^2/var_fore[i])}
-  
   return (-sum)
 }
+# initial value
 initialvalue=c(9.614052e-05,0.5,0.2)
+# optimization process
 result3 = optim(initialvalue, fn=function(theta){sum_pdf(Return,theta)})
-
+# show result
 sqrt(longtermvar)
 result3$par[2]
 result3$par[3]
 sqrt(result3$par[1])
-
 result3
 
-#4
-
+#4: Two parameters to estimate. Set initial and long-run variance equalt to sample variance over the last 1000 days.
 sum_pdf <- function(Return,theta)
 {
-  long_var=longtermvar;alpha=theta[1];beta=theta[2];var1=longtermvar#sigma is long term variance
+  long_var=longtermvar;alpha=theta[1];beta=theta[2];var1=initialvar
   var_fore=rep(0,1000)
   var_fore[1]=var1
   for (i in 2:1000)
@@ -110,24 +108,24 @@ sum_pdf <- function(Return,theta)
   sum = 0
   for (i in 1:1000)
   {sum = sum - 0.5*(log(var_fore[i]*2*pi)+Return[i]^2/var_fore[i])}
-  
   return (-sum)
 }
+# initial value
 initialvalue=c(0.5,0.2)
+# optimization process
 result4 = optim(initialvalue, fn=function(theta){sum_pdf(Return,theta)})
-
+# show result
 sqrt(longtermvar)
 result4$par[1]
 result4$par[2]
 sqrt(longtermvar)
-
 result4
 
-#5
-#(a)
+#5: forecase the return variance and standared deviation for Friday, August 15, 2014
+#a: forecast under model 1
 estimation1=function(theta)
 {
-  var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]#sigma is long term variance
+  var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]
   var_fore=rep(0,1001)
   var_fore[1]=var1
   for (i in 2:1001)
@@ -136,15 +134,13 @@ estimation1=function(theta)
   }
   return(var_fore[1001]^0.5)
 }
-
 estimationA=estimation1(c(result1$par[1],result1$par[2],result1$par[3],result1$par[4]))
-
 estimationA
 
-#(b)
+#b: forecast under model 2
 estimation2=function(theta)
 {
-  var1=theta[1];alpha=theta[2];beta=theta[3];long_var=longtermvar#sigma is long term variance
+  var1=theta[1];alpha=theta[2];beta=theta[3];long_var=longtermvar
   var_fore=rep(0,1001)
   var_fore[1]=var1
   for (i in 2:1001)
@@ -153,15 +149,13 @@ estimation2=function(theta)
   }
   return(var_fore[1001]^0.5)
 }
-
 estimationB=estimation2(c(result2$par[1],result2$par[2],result2$par[3]))
-
 estimationB
 
-#(c)
+#c: forecast under model 3
 estimation3=function(theta)
 {
-  var1=longtermvar;alpha=theta[2];beta=theta[3];long_var=theta[1]#sigma is long term variance
+  var1=longtermvar;alpha=theta[2];beta=theta[3];long_var=theta[1]
   var_fore=rep(0,1001)
   var_fore[1]=var1
   for (i in 2:1001)
@@ -170,12 +164,10 @@ estimation3=function(theta)
   }
   return(var_fore[1001]^0.5)
 }
-
 estimationC=estimation3(c(result3$par[1],result3$par[2],result3$par[3]))
-
 estimationC
 
-#(d)
+#d: forecast under model 4
 estimation4=function(theta)
 {
   var1=longtermvar;alpha=theta[1];beta=theta[2];long_var=longtermvar#sigma is long term variance
@@ -187,94 +179,75 @@ estimation4=function(theta)
   }
   return(var_fore[1001]^0.5)
 }
-
 estimationD=estimation4(c(result4$par[1],result4$par[2]))
-
 estimationD
 
-#2_
-#(a)
+#2: forecase of the realized variance from Aug. 15, 2014 to Sept. 19, 2014 (24 days)
+#a: Model 1
 estimation2_1=function(theta)
 {
-  var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]#sigma is long term variance
+  var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]
   var_fore=rep(0,24)
   var_fore[1] = long_var*(1-alpha-beta)+(alpha+beta)*var1
-  
   for (i in 2:24)
   {
     var_fore[i] = long_var*(1-alpha-beta)+(alpha+beta)*var_fore[i-1]
-    
   }
   sum=sum(var_fore)
   return(sum)
 }
-
 estimation2_A=estimation2_1(c(estimationA^2,result1$par[2],result1$par[3],result1$par[4]))
-
 estimation2_A
 sqrt(estimation2_A*252/24)
 
-
-#(b)
+#b: Model 2
 estimation2_2=function(theta)
 {
   var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]#sigma is long term variance
   var_fore=rep(0,24)
   var_fore[1] = long_var*(1-alpha-beta)+(alpha+beta)*var1
-  
   for (i in 2:24)
   {
     var_fore[i] = long_var*(1-alpha-beta)+(alpha+beta)*var_fore[i-1]
-    
   }
   sum=sum(var_fore)
   return(sum)
 }
-
 estimation2_B=estimation2_2(c(estimationB^2,result2$par[2],result2$par[3],longtermvar))
-
 estimation2_B
 sqrt(estimation2_B*252/24)
 
-#(c)
+#c: Model 3
 estimation2_3=function(theta)
 {
   var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]#sigma is long term variance
   var_fore=rep(0,24)
   var_fore[1] = long_var*(1-alpha-beta)+(alpha+beta)*var1
-  
   for (i in 2:24)
   {
     var_fore[i] = long_var*(1-alpha-beta)+(alpha+beta)*var_fore[i-1]
-    
   }
   sum=sum(var_fore)
   return(sum)
 }
-
 estimation2_C=estimation2_3(c(estimationC^2,result3$par[2],result3$par[3],result3$par[1]))
-
 estimation2_C
 sqrt(estimation2_C*252/24)
 
-#(d)
+#d: Model 4
 estimation2_4=function(theta)
 {
   var1=theta[1];alpha=theta[2];beta=theta[3];long_var=theta[4]#sigma is long term variance
   var_fore=rep(0,24)
   var_fore[1] = long_var*(1-alpha-beta)+(alpha+beta)*var1
-  
   for (i in 2:24)
   {
     var_fore[i] = long_var*(1-alpha-beta)+(alpha+beta)*var_fore[i-1]
-    
   }
   sum=sum(var_fore)
   return(sum)
 }
-
 estimation2_D=estimation2_4(c(estimationD^2,result4$par[1],result4$par[2],longtermvar))
-
 estimation2_D
 sqrt(estimation2_D*252/24)
 
