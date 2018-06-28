20e3c3cd079a1dff6681ad614499dbb0e0651114
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-28 09:20:32 -0500

    Update DCC - new.R

--- DCC - new.R
+++ DCC - new.R
@@ -1,9 +1,7 @@
 Data = read.csv("/Users/Larry/Documents/R_Code/DCC/HW8.csv")
 Return_IWM = log(Data$IWM.P[2:nrow(Data)] / Data$IWM.P[1:(nrow(Data)-1)])
 Return_EFA = log(Data$EFA.P[2:nrow(Data)] / Data$EFA.P[1:(nrow(Data)-1)])
-
 library(foreach)
-
 t = length(Return_IWM)
 
 Loglikelihood <- function(Return, Theta)
@@ -11,21 +9,16 @@ Loglikelihood <- function(Return, Theta)
   if ( sum(Theta[1:4] <0) != 0 || Theta[1] * (1+Theta[5]^2) + Theta[2] > 1)
   {
     return (10000)
-    
   }
   t = length(Return)
   Var = rep(0,t)
   Var[1] = Theta[4]^2
-  
   for(i in 2:t)
   {
     Var[i] = (1- Theta[1] * (1+ Theta[5]^2) - Theta[2]) * Theta[3]^2 + Theta[1] * (Return[i-1] - Theta[5] * sqrt(Var[i-1]))^2 + Theta[2] * Var[i-1]
-    
   }
-  
   LN = 0.5* sum(log(Var)+Return^2/Var)
   return(LN)
-  
 }
 
 iIWM = c(0.1,0.88,sd(Return_IWM),sd(Return_IWM),0)
@@ -47,7 +40,6 @@ DCCLN <- function(Return1, Return2, Theta){
   Q11=rep(0,t)
   Q12=rep(0,t)
   Q22=rep(0,t)
-  
   Q11[1]=1
   Q12[1]=rho12_hist
   Q22[1]=1
@@ -56,15 +48,12 @@ DCCLN <- function(Return1, Return2, Theta){
     Q11[i] = 1 + Theta[1] * (Return1[i-1]^2 - 1) + Theta[2] * (Q11[i-1] - 1)
     Q12[i] = rho12_hist + Theta[1] * (Return1[i-1] * Return2[i-1] - rho12_hist) + Theta[2] * (Q12[i-1] - rho12_hist)
     Q22[i] = 1 + Theta[1] * (Return2[i-1]^2-1)+ Theta[2] * (Q22[i-1]-1)
-    
   }
   
   rho12 = Q12/sqrt(Q11*Q22)
   temp = 1 - rho12^2
   LN = 0.5* sum(log(temp)+(Return1^2 + Return2^2 - 2 * rho12 * Return1*Return2) / temp)
-  
   return(LN)
-  
 }
 
 #Standadizing
@@ -77,19 +66,16 @@ Vol_EFA[1] = Par_EFA[4]
 for (i in 2:t){
   Vol_IWM[i] = sqrt((1 - Par_IWM[1] *(1+ Par_IWM[5]^2)-Par_IWM[2]) * Par_IWM[3]^2 + Par_IWM[1] * (Return_IWM[i-1] - Par_IWM[5] * Vol_IWM[i-1])^2 + Par_IWM[2] * Vol_IWM[i-1]^2)
   Vol_EFA[i] = sqrt((1 - Par_EFA[1] *(1+ Par_EFA[5]^2)-Par_EFA[2]) * Par_EFA[3]^2 + Par_EFA[1] * (Return_EFA[i-1] - Par_EFA[5] * Vol_EFA[i-1])^2 + Par_EFA[2] * Vol_EFA[i-1]^2)
-  
 }
 
 z_IWM = Return_IWM/Vol_IWM
 z_EFA = Return_EFA/Vol_EFA
 length(Vol_IWM)
-
 #DCC MLE
 iDCC = c(0.05, 0.9)
 result_DCC = optim(iDCC, DCCLN, Return1= z_IWM, Return2 = z_EFA, method = "BFGS")
 Par_DCC = result_DCC$par
 result_DCC
-
 #DCC of Rho
 hist_rho = mean(z_IWM*z_EFA)
 q11=1
@@ -100,33 +86,25 @@ for (i in 2:t){
   q12 = hist_rho + Par_DCC[1]*(z_IWM[i-1]*z_EFA[i-1]-hist_rho) + Par_DCC[2] * (q12 - hist_rho)
   q22 = 1 + Par_DCC[1] * (z_EFA[i-1]^2-1) + Par_EFA[2]*(q22-1)
 }
-
 rho12 = q12/sqrt(q11*q22)
 
 #Monte Carlo
-
 #Working days number
 WD = 256
-
 # Price of Aug. 5 2014
 IWM_ini = Data$IWM.P[t+1]
 EFA_ini = Data$EFA.P[t+1]
-
 MCs = foreach(i = 1:5000) %do%
 {
-  
   D = diag(c(Vol_IWM[t],Vol_EFA[t]))
   Upsilon = matrix(c(1,rho12,rho12,1),2,2)
   QQ11 = q11
   QQ12 = q12
   QQ22 = q22
-  
   acumu_IWM = rep(0,WD)
   acumu_EFA = rep(0,WD)
-  
   acumu_IWM[1]=Return_IWM[t]
   acumu_EFA[1]=Return_EFA[t]
-  
   for (i in 2:WD)
   {
     rho = Upsilon[1,2]
@@ -136,7 +114,6 @@ MCs = foreach(i = 1:5000) %do%
     r2 = acumu_EFA[i-1]
     v1 = D[1,1]
     v2 = D[2,2]
-    
     z1 = r1/v1
     z2 = r2/v2
     
@@ -151,12 +128,9 @@ MCs = foreach(i = 1:5000) %do%
     
     D = diag(c(v1,v2))
     Upsilon = matrix(c(1,rho,rho,1),2,2)
-    
     r= D %*% cholesky_Upsilon %*% rnorm(2)
-    
     acumu_IWM[i] = r[1]
     acumu_EFA[i] = r[2]
-    
   }
     Estimate_IWM = IWM_ini * exp(sum(acumu_IWM))
     Estimate_EFA = EFA_ini * exp(sum(acumu_EFA))
@@ -164,9 +138,7 @@ MCs = foreach(i = 1:5000) %do%
   #Butters
     Butter_IWM = (IWM_ini-Estimate_IWM)/IWM_ini
     Butter_EFA = (EFA_ini-Estimate_EFA)/EFA_ini
-    
     Worst = max(Butter_EFA,Butter_IWM)
-    
     if (Worst <= 0.15)
     {
       P=1000
@@ -175,20 +147,15 @@ MCs = foreach(i = 1:5000) %do%
     {
       P = 1000 - 1000* (Worst -0.15) * 1.1765
     }
-  
   return (P)
-  
 }
 
 #Pricing, discount of each coupon payment, 12 month libor : 0.0056
 PV_coupon = 0
 MCs = unlist(MCs)
-
 for (i in 1:12){
   PV_coupon = PV_coupon + 4.1833/(1+0.0056*(3+i*30)/360)
   }
-
 Value_Present = PV_coupon + mean(MCs)/(1+0.0056*370/360)
-
 Value_Present
 
