1393fcf259488a82ddd1e84254386c5c2f3254bb
Dongliang (Larry) Yi <larry.ydl@gmail.com>
2018-06-28 22:11:36 -0500

    Update VaR_Cal_4methods.R

--- VaR_Cal_4methods.R
+++ VaR_Cal_4methods.R
@@ -1,9 +1,10 @@
-data =read.csv("/Users/Larry/Google Drive/Risk Management/HW2/580HW2.csv",header=T)
+# Value-at-Risk Computed Using Various Methods
+# totally 2000 + 246 days, we need to derive return distribution from the first 2000 days
 
-#thousand.000
+data =read.csv("/Users/Larry/Google Drive/Risk Management/HW2/580HW2.csv",header=T)
 VaR <- vector("numeric", length=246)
 
-#totally 2000 + 246 days, we need to derive return distribution from the first 2000 days
+#Value at Risk (VaR) with historical simulation method
 for (i in 1:246)
 {
 PLGBP = (1000/(data$GBPUSD[i:(i+1999)]*data$UKX[i:(i+1999)]))*data$GBPUSD[(i+1):(i+2000)]*data$UKX[(i+1):(i+2000)]-1000
@@ -14,7 +15,7 @@ VaR[i]<-abs(unname(quantile(Total,0.01,na.rm=T)))
 }
 VaR
 
-#VaR stores the historical VaR
+#Value at Risk (VaR) with delta-normal method 
 VaRdelta <- vector("numeric", length=246)
 for (i in 1:246)
 {
@@ -31,23 +32,8 @@ for (i in 1:246)
   VaRdelta[i]=2.33*stdPortfolio*1000
 }
 VaRdelta
-VaRdeltabook <- vector("numeric", length=246)
 
-for (i in 1:246)
-{
-  RFTGBP = (1/(data$GBPUSD[i:(i+1999)]*data$UKX[i:(i+1999)]))*data$GBPUSD[(i+1):(i+2000)]*data$UKX[(i+1):(i+2000)]-1
-  RDAXEUR = (1/(data$EURUSD[i:(i+1999)]*data$DAX[i:(i+1999)]))*data$EURUSD[(i+1):(i+2000)]*data$DAX[(i+1):(i+2000)]-1
-  RSP500 = (1/data$SPX[i:(i+1999)])*data$SPX[(i+1):(i+2000)]-1
-  stdRSP500 = sqrt((sum(RSP500^2))/2000)
-  stdRDAXEUR = sqrt((sum(RDAXEUR^2))/2000)
-  stdRFTGBP = sqrt((sum(RFTGBP^2))/2000)
-  covSPFT = (sum(RSP500*RFTGBP))/2000
-  covSPDA = (sum(RSP500*RDAXEUR))/2000
-  covFTDA = (sum(RFTGBP*RDAXEUR))/2000
-  stdPortfolio=sqrt((sum(RSP500^2))/2000+(sum(RDAXEUR^2))/2000+(sum(RFTGBP^2))/2000+2*covSPFT+2*covSPDA+2*covFTDA)
-  VaRdeltabook[i]=2.33*stdPortfolio*1000
-}
-VaRdeltabook
+#Value at Risk (VaR) with delta-normal method using the exponentially weighted covariance estimator
 VaRdeltaexpo <- vector("numeric", length=246)
 weight <- vector("numeric", length=100)
 weight[100]=0.06
@@ -55,8 +41,7 @@ for (i in 1:99)
 {
   weight[i]=(0.94^(100-i))*0.06
 }
- 
-for (i in 1:246)
+ for (i in 1:246)
 {
   RFTGBP = (1/(data$GBPUSD[(i+1900):(i+1999)]*data$UKX[(i+1900):(i+1999)]))*data$GBPUSD[(i+1901):(i+2000)]*data$UKX[(i+1901):(i+2000)]-1
   RDAXEUR = (1/(data$EURUSD[(i+1900):(i+1999)]*data$DAX[(i+1900):(i+1999)]))*data$EURUSD[(i+1901):(i+2000)]*data$DAX[(i+1901):(i+2000)]-1
@@ -72,12 +57,13 @@ for (i in 1:246)
 }
 VaRdeltaexpo
 
+#Value at Risk (VaR) with historical simulation using rescalled returns
 data2 =read.csv("/Users/Larry/Desktop/580HW2(100lag).csv",header=T)
 VaRexpohist <- vector("numeric", length=246)
 sigmaFT <- vector("numeric", length=2246)
 sigmaDAX<- vector("numeric", length=2246)
 sigmaSP <- vector("numeric", length=2246)
-
+# calculate volatility which will be used to rescale returns
 for (i in 1:2247)
 {
   RFTGBP = (1/(data2$GBPUSD[i:(i+99)]*data2$UKX[(i):(i+99)]))*data2$GBPUSD[(i+1):(i+100)]*data2$UKX[(i+1):(i+100)]-1
@@ -87,7 +73,7 @@ for (i in 1:2247)
   sigmaDAX[i] = sqrt(sum((RDAXEUR^2)*weight))
   sigmaFT[i] = sqrt(sum((RFTGBP^2)*weight))
 }
-
+# rescale returns
 for (i in 1:246)
 {
   PLGBP = ((1000/(data$GBPUSD[i:(i+1999)]*data$UKX[i:(i+1999)]))*data$GBPUSD[(i+1):(i+2000)]*data$UKX[(i+1):(i+2000)]-1000)*sigmaFT[i+2000]/sigmaFT[(i+1):(i+1999)]
@@ -97,6 +83,8 @@ for (i in 1:246)
   VaRexpohist[i]<-abs(unname(quantile(Total,0.01,na.rm=T)))
 }
 VaRexpohist
+
+# plot the result
 Date=as.Date(data$Date,"%m/%d/%Y")
 plot(Date[2002:2247],VaR,type="l")
 plot(Date[2002:2247],VaRdeltabook,type="l",add=T)
